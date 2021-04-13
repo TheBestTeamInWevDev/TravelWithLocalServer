e71173b4208ec03b481b24e0d242fcb40eb46771
Alanbition <wang.shij@husky.neu.edu>
2021-04-12 21:02:57 -0400

    Push from master branch

---
+++ src/test/java/com/example/wbdvtravel_with_local/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvtravel_with_local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
